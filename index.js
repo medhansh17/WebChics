56800a84360002d7c7730ca5b92569ce66e67b4e
Medhansh Jain <medhanshjain2010@gmail.com>
2023-09-16 17:27:53 +0530

    Added hamburger menu

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-// var prevScrollpos = window.scrollY;
-// window.onscroll = function () {
-//   var currentScrollPos = window.scrollY;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("nav-bar").style.top = "0";
-//   } else {
-//     document.getElementById("nav-bar").style.top = "-80px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// };
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
